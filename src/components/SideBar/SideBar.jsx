8e53b2d195c4807317145e48654ea49c42ecd075
elikese <elikese@gmail.com>
2024-02-22 16:45:28 +0900

    feat : sidebar 완성, menu setting

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,40 @@
 /** @jsxImportSource @emotion/react */
 
-import { layout } from "./style"
-import { FaCaretRight } from "react-icons/fa"
+import { useMemo, useState } from "react";
+import * as S from "./style"
+import { FaCaretRight, FaCaretLeft } from "react-icons/fa"
+
+
 
 
 function SideBar() {
+  const [isShow, setShow] = useState(false);
+  const menus = useMemo(() =>
+    [
+      {
+        id: 1,
+        path: "/mypage",
+        name: "마이페이지"
+      },
+      {
+        id: 2,
+        path: "/board",
+        name: "게시글"
+      },
+      {
+        id: 3,
+        path: "/notice",
+        name: "공지사항"
+      },
+    ], []);
   return (
-    <aside css={layout}>
-      <button><FaCaretRight /></button>
+    <aside css={S.layout(isShow)}>
+      <button css={S.toggleButton} onClick={() => { setShow(!isShow) }}>
+        {isShow ? <FaCaretLeft /> : <FaCaretRight />}
+      </button>
+      <ul>
+
+      </ul>
     </aside>
   )
 }
--- src/components/SideBar/style.js
+++ src/components/SideBar/style.js
@@ -1,12 +1,44 @@
 import { css } from "@emotion/react";
 
-export const layout = css`
-position: fixed;
-top: 0;
-left: 0;
+export const layout = (isShow) => css`
+  position: fixed;
+  top: 0;
+  left: ${isShow ? "0px" : "-300px"};
 
-width: 300px;
-height: 100%;
+  width: 300px;
+  height: 100%;
+  transition: left 0.7s ease-in-out;
+  background-color: black;
+`;
 
-background-color: black;
+export const toggleButton = css`
+  box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: -15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  width: 15px;
+  height: 50px;
+
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+
+  padding: 0;
+  border: 1px solid #dbdbdb;
+  background-color: white;
+
+  transition: all 0.5s ease-in-out;
+
+  cursor: pointer;
+  &:hover {
+    background-color: #eee;
+  }
+  &:active {
+    background-color: #ddd;
+  }
 `;
